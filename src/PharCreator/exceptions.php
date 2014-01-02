c74bb5560a1dfa9a1c7b431fe5615ced80abedc0
Budry <zarubaondra@gmail.com>
2014-01-02 23:23:20 +0100

    Added directory not found exception

---
+++ src/PharCreator/exceptions.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This file is part of the PharCreator package.
+ *
+ * (c) Ondřej Záruba <zarubaondra@gmail.com>
+ *
+ * For the full copyright and license information, please view the license.md
+ * file that was distributed with this source code.
+ */
+
+namespace PharCreator;
+
+/**
+ * Class DirectoryNotFoundException
+ * @package PharCreator
+ */
+class DirectoryNotFoundException extends \LogicException
+{
+
+}
